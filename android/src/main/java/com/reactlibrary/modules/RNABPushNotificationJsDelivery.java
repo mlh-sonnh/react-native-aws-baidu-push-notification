b0720f5e11115901216c611a76208e12ba4eea91
sonnh <sonnh@lifetimetech.vn>
2020-03-27 08:59:58 +0700

    add local push notification

--- android/src/main/java/com/reactlibrary/modules/RNABPushNotificationJsDelivery.java
+++ android/src/main/java/com/reactlibrary/modules/RNABPushNotificationJsDelivery.java
@@ -1,5 +1,6 @@
 package com.reactlibrary.modules;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -7,6 +8,11 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Set;
 
 public class RNABPushNotificationJsDelivery {
     private ReactApplicationContext context;
@@ -49,4 +55,56 @@ public class RNABPushNotificationJsDelivery {
                     .emit(eventName, params);
         }
     }
+
+    public void notifyRemoteFetch(Bundle bundle) {
+        String bundleString = convertJSON(bundle);
+        WritableMap params = Arguments.createMap();
+        params.putString("dataJSON", bundleString);
+        sendEvent("remoteFetch", params);
+    }
+
+    public void notifyNotification(Bundle bundle) {
+        String bundleString = convertJSON(bundle);
+
+        WritableMap params = Arguments.createMap();
+        params.putString("dataJSON", bundleString);
+
+        sendEvent("remoteNotificationReceivedBaidu", params);
+    }
+
+    void notifyNotificationAction(Bundle bundle) {
+        String bundleString = convertJSON(bundle);
+
+        WritableMap params = Arguments.createMap();
+        params.putString("dataJSON", bundleString);
+
+        sendEvent("notificationActionReceived", params);
+    }
+
+    String convertJSON(Bundle bundle) {
+        try {
+            JSONObject json = convertJSONObject(bundle);
+            return json.toString();
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    // a Bundle is not a map, so we have to convert it explicitly
+    JSONObject convertJSONObject(Bundle bundle) throws JSONException {
+        JSONObject json = new JSONObject();
+        Set<String> keys = bundle.keySet();
+        for (String key : keys) {
+            Object value = bundle.get(key);
+            if (value instanceof Bundle) {
+                json.put(key, convertJSONObject((Bundle)value));
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                json.put(key, JSONObject.wrap(value));
+            } else {
+                json.put(key, value);
+            }
+        }
+        return json;
+    }
+
 }
